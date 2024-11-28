d85d27090b3fc9b890d5cb0eede18f23c95af263
anasooo <anassodor1@gmail.com>
2024-11-28 08:59:08 +0100

    Initialization, done

--- philo/initialization.c
+++ philo/initialization.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 06:11:49 by asodor            #+#    #+#             */
-/*   Updated: 2024/11/28 07:14:55 by asodor           ###   ########.fr       */
+/*   Updated: 2024/11/28 08:55:18 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void ft_set_philo(t_process *process, t_philo *philo, t_fork **forks, lon
     philo->r_fork = forks[(i + 1) % process->n_philos];
 }
 
-t_philo **init_philos(t_process *process, t_fork **forks)
+static t_philo **init_philos(t_process *process, t_fork **forks)
 {
     t_philo **philos;
     long    i;
@@ -43,7 +43,7 @@ t_philo **init_philos(t_process *process, t_fork **forks)
     return (philos);
 }
 
-t_fork **init_forks(int n_philos)
+static t_fork **init_forks(int n_philos)
 {
     t_fork  **forks;
     long     i;
@@ -65,9 +65,24 @@ t_fork **init_forks(int n_philos)
     return (forks);
 }
 
+static bool init_process(t_process *process, t_fork **forks, t_philo **philos)
+{   process->ready = false;
+    process->philo_died = false;
+    process->err = false;
+    process->finished = 0;
+    process->forks = forks;
+    process->philos = philos;
+    if (pthread_mutex_init(&process->mutex, NULL))
+        return (free(process),false);
+    if (pthread_mutex_init(&process->print_mutex, NULL))
+        return (free(process),false);
+    return (true);
+}
 
 void    ft_initialization(t_process *process, t_fork **forks, t_philo **philos)
 {
+    bool p;
+    
     forks = init_forks(process->n_philos);
     if (!forks)
     {
@@ -79,6 +94,12 @@ void    ft_initialization(t_process *process, t_fork **forks, t_philo **philos)
     {
         ft_putendl_fd("Philos initialization failed!\n", STDERR_FILENO);
         return (free(process));
+    }
+    p = init_process(process, forks, philos);
+    if (!p)
+    {
+        ft_putendl_fd("Process initialization failed!\n", STDERR_FILENO);
+        return ;
     }
 }
 

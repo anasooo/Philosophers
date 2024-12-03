15d8504b12e0cfadb7a2ba39e7871d23de3dd382
anasooo <anassodor1@gmail.com>
2024-12-03 13:59:23 +0100

    get time

--- philo/ft_time.c
+++ philo/ft_time.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 06:25:12 by asodor            #+#    #+#             */
-/*   Updated: 2024/11/29 09:36:58 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/03 13:51:58 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,5 @@ unsigned long	ft_get_time(void)
     struct timeval	time;
 
     gettimeofday(&time, NULL);
-    return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
-}
-
-void	ft_timer(t_process *process)
-{
-    unsigned long	start;
-
-    start = ft_get_time();
-    while (ft_get_time() - start < process->time->to_die)
-        usleep(100);
+    return ((unsigned long)(time.tv_sec * 1000) + (time.tv_usec / 1000));
 }

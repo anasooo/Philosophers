b3194dd4ee1b0637b95090f4db39915daf93e4ac
anasooo <anassodor1@gmail.com>
2024-12-03 17:26:34 +0100

    take forks

--- philo/routine.c
+++ philo/routine.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 13:59:56 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/03 15:16:25 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/03 17:25:51 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,47 @@ static bool	all_is_ready(t_process *process)
     return (true);
 }
 
+bool	take_r_fork(t_philo *philo, t_process *process)
+{   
+    if (process->err || process->philo_died)
+		return (false);
+	ft_print(philo, 'f');
+	return (true);
+}
+
+bool	take_l_fork(t_philo *philo, t_process *process)
+{
+    if (process->err || process->philo_died)
+        return (false);
+    ft_print(philo, 'f');
+    return (true);
+}
+
+static bool ft_take_forks(t_process *process, t_philo *philo)
+{
+    if (philo->id % 2 == 0)
+        return (take_r_fork(philo, process) && take_l_fork(philo, process));
+    else
+        return (take_l_fork(philo, process) && take_r_fork(philo, process));
+}
+
+static bool	simulation(t_process *process, t_philo *philo)
+{
+    long m;
+    
+    if (philo->id % 2)
+        usleep(10);
+    m = 0;
+    while (m < process->n_meals || process->n_meals == -1)
+    {
+        if (!ft_take_forks(process, philo))
+            return (false);
+        if (!ft_eat(process, philo))
+            return (false);
+    }
+    
+}
+
 void	*ft_routine(void *data)
 {
     t_philo	*philo;
@@ -32,7 +73,11 @@ void	*ft_routine(void *data)
 
     philo = (t_philo *)data;
     process = philo->process;
+    
+    //wait for all threads to be ready
     if (!all_is_ready(process))
+        return (NULL);
+    if (!simulation(process, philo))
         return (NULL);
     
     return (NULL);

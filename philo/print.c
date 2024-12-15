34ea9a8a2ebb54d2a8b1b8af819018ce0988b01c
anasooo <anassodor1@gmail.com>
2024-12-15 03:34:19 +0100

    eating state

--- philo/print.c
+++ philo/print.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 02:16:26 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/15 02:29:00 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/15 03:11:17 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,7 @@ void    ft_print_fork(t_philo *philo)
     printf("%u %d %s", ft_get_time(), philo->id, FORK);
 }
 
+void    ft_print_eating(t_philo *philo)
+{
+    printf("%u %d %s", ft_get_time(), philo->id, EAT);
+}

7ca9e72e58b5f5374c6b8418ee6afbf888d4ad69
anasooo <anassodor1@gmail.com>
2024-12-15 03:35:44 +0100

    set philo last eat

--- philo/setters.c
+++ philo/setters.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 12:55:08 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/03 12:57:52 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/15 03:01:52 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,10 @@
 void	set_ready(t_process *process)
 {
     process->ready = true;
+}
+
+int set_philo_last_eat(t_philo *philo)
+{
+    philo->last_eat = ft_get_time();
+    return 0;
 }

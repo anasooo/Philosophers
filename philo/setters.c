798747466076dd16d7a2b7a52f3d07cea354fc3e
anasooo <anassodor1@gmail.com>
2024-12-17 11:30:09 +0100

    fix: change set_philo_last_eat to return void and add functions to set finished state and update process state

--- philo/setters.c
+++ philo/setters.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 12:55:08 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/15 03:01:52 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/17 09:15:09 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,16 @@ void	set_ready(t_process *process)
     process->ready = true;
 }
 
-int set_philo_last_eat(t_philo *philo)
+void set_philo_last_eat(t_philo *philo)
 {
     philo->last_eat = ft_get_time();
-    return 0;
+}
+
+void ft_set_philo_finished(t_philo *philo)
+{
+    philo->finished = 1;
+}
+void ft_update_process_state(t_process *process)
+{
+    process->finished++;
 }

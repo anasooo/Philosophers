f108518e389ef140dc510bed22999504a6fd7c54
anasooo <anassodor1@gmail.com>
2024-12-18 22:28:36 +0100

    fix: add mutex locking in philo finished state and process state updates

--- philo/setters.c
+++ philo/setters.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 12:55:08 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/18 20:08:33 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/18 22:11:57 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,13 @@ void set_philo_last_eat(t_philo *philo)
 
 void ft_set_philo_finished(t_philo *philo)
 {
+    pthread_mutex_lock(&philo->mutex);
     philo->finished = 1;
+    pthread_mutex_unlock(&philo->mutex);
 }
 void ft_update_process_state(t_process *process)
 {
+    pthread_mutex_lock(&process->mutex);
     process->finished++;
+    pthread_mutex_unlock(&process->mutex);
 }

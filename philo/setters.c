5b887e1ca7ce0be3983160f04b0345b3397ea7d3
anasooo <anassodor1@gmail.com>
2024-12-19 16:16:47 +0100

    fix: add mutex locking around process

--- philo/setters.c
+++ philo/setters.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 12:55:08 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/18 22:11:57 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/19 10:44:44 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 void	set_ready(t_process *process)
 {
+    pthread_mutex_lock(&process->mutex);
     process->ready = true;
+    pthread_mutex_unlock(&process->mutex);
 }
 
 void set_philo_last_eat(t_philo *philo)

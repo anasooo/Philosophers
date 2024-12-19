ed52c6b2a37eb2f86536a62022800faae8f1f9cf
anasooo <anassodor1@gmail.com>
2024-12-19 16:15:50 +0100

    fix: update input parsing to use ft_atoui and handle error checks

--- philo/parsing.c
+++ philo/parsing.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 00:20:27 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/17 11:19:06 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/19 16:06:02 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ static t_process *check_n_meals(int ac, t_process *process, char **av, bool *err
 {
     if (ac == 5)
     {
-        process->number_of_meals = ft_atoul(av[4], error);
-        return (free(process), NULL);
+        process->number_of_meals = ft_atoui(av[4], error);
+        if(*error)
+            return (free(process), NULL);
     }    
     else
         process->number_of_meals = -1;
@@ -28,7 +29,7 @@ t_process	*ft_parse_input(int ac, char **av)
     t_process	*process;
     bool       error;
     
-    error = 0;
+    error = false;
     process = (t_process *)malloc(sizeof(t_process));
     if (!process)
         return (ft_putendl_fd("Allocation failed!\n", STDERR_FILENO), NULL);
@@ -36,15 +37,15 @@ t_process	*ft_parse_input(int ac, char **av)
     process->number_of_philos = ft_atoui(av[0], &error);
     if(error)
         return (free(process), NULL);
-    process->time->to_die = ft_atoul(av[1], &error);
+    process->time.to_die = ft_atoui(av[1], &error);
     if(error)
         return (free(process), NULL);
-    process->time->to_eat = ft_atoul(av[2], &error);
+    process->time.to_eat = ft_atoui(av[2], &error);
     if(error)
         return (free(process), NULL);
-    process->time->to_sleep = ft_atoul(av[3], &error);
+    process->time.to_sleep = ft_atoui(av[3], &error);
     if(error)
         return (free(process), NULL);
-    return (check_n_meals(ac, process, av, error));
+    return (check_n_meals(ac, process, av, &error));
 }
 

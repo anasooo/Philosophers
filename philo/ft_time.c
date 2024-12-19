dc844ce3cd394226dd7559ae4ab9119c85292daf
anasooo <anassodor1@gmail.com>
2024-12-19 16:15:11 +0100

    fix: change return type of ft_get_time and ft_usleep

--- philo/ft_time.c
+++ philo/ft_time.c
@@ -6,20 +6,20 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 06:25:12 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/15 03:22:02 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/19 13:20:26 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h" 
 
-unsigned long	ft_get_time(void)
+unsigned int	ft_get_time(void)
 {
     struct timeval	time;
 
     gettimeofday(&time, NULL);
     return ((unsigned long)(time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
-ft_usleep(unsigned long time)
+int ft_usleep(unsigned long time)
 {
     unsigned long start;
 

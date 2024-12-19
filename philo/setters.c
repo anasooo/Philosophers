84d5e8d7b414beda9f0780b573ef32f0993d645e
anasooo <anassodor1@gmail.com>
2024-12-19 20:39:07 +0100

    feat: add death print function and implement all_is_ready for process readiness

--- philo/setters.c
+++ philo/setters.c
@@ -6,11 +6,24 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 12:55:08 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/19 10:44:44 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/19 17:27:46 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	all_is_ready(t_process *process)
+{
+    int is_ready;
+    
+    is_ready = 0;
+    while (!is_ready)
+    {
+        pthread_mutex_lock(&process->mutex);
+        is_ready = process->ready;
+        pthread_mutex_unlock(&process->mutex);
+    }
+}
 
 void	set_ready(t_process *process)
 {
@@ -32,7 +45,7 @@ void ft_set_philo_finished(t_philo *philo)
     philo->finished = 1;
     pthread_mutex_unlock(&philo->mutex);
 }
-void ft_update_process_state(t_process *process)
+void ft_set_count(t_process *process)
 {
     pthread_mutex_lock(&process->mutex);
     process->finished++;

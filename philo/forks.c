5cdc20e2f7b34ac788311a5702b09ea505861200
anasooo <anassodor1@gmail.com>
2024-12-19 16:13:39 +0100

    fix: update return status and improve error checking

--- philo/forks.c
+++ philo/forks.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 02:32:13 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/18 22:01:14 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/19 13:13:39 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,53 +21,51 @@ int ft_check_philo_died(t_process *process)
     return (died);
 }
 
-void	take_r_fork(t_philo *philo, t_process *process)
+int	take_r_fork(t_philo *philo, t_process *process)
 {
     if (ft_check_philo_died(process) != 0)
-        return (NULL);
+        return (0);
     pthread_mutex_lock(&philo->r_fork->mutex);
     if (ft_check_philo_died(process) != 0)
     {
         pthread_mutex_unlock(&philo->r_fork->mutex);
         if (philo->id % 2 != 0)
             pthread_mutex_unlock(&philo->l_fork->mutex);
-        return (NULL);
+        return (0);
     }
 	ft_print_fork(philo);
+    return (1);
 }
 
-void	take_l_fork(t_philo *philo, t_process *process)
+int	take_l_fork(t_philo *philo, t_process *process)
 {
     if (ft_check_philo_died(process) != 0)
-        return (NULL);
+        return (0);
     pthread_mutex_lock(&philo->l_fork->mutex);
     if(ft_check_philo_died(process) != 0)
     {
         pthread_mutex_unlock(&philo->l_fork->mutex);
         if (philo->id % 2 == 0)
             pthread_mutex_unlock(&philo->l_fork->mutex);
-        return (NULL);
+        return (0);
     }
     ft_print_fork(philo);
+    return (1);
 }
 
-void  ft_take_forks(t_process *process, t_philo *philo)
+int  ft_take_forks(t_process *process, t_philo *philo)
 {
     if (philo->id % 2 == 0)
-    {
-        take_r_fork(philo, process);
-        take_l_fork(philo, process);
-    }else
-    {
-        take_l_fork(philo, process);
-        take_r_fork(philo, process);
-    }
+        return (take_r_fork(philo, process) && take_l_fork(philo, process));
+    else
+        return (take_l_fork(philo, process) && take_r_fork(philo, process));
 }
 
-void ft_put_forks(t_process *process, t_philo *philo)
+int ft_put_forks(t_process *process, t_philo *philo)
 {
     pthread_mutex_unlock(&philo->l_fork->mutex);
     pthread_mutex_unlock(&philo->r_fork->mutex);
     if (ft_check_philo_died(process) != 0)
-        return (NULL);
+        return (0);
+    return (1);
 }

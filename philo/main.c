b0ca6783986a8ed83735af139dfe50f2d5ddd1ee
anasooo <anassodor1@gmail.com>
2024-12-21 23:02:01 +0100

    fix: remove unnecessary parameters from ft_initialization and update fork and philosopher freeing logic

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 22:22:24 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/19 16:04:35 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/21 16:49:53 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,18 @@
 int	main(int ac, char **av)
 {
 	t_process	*process;
-	t_fork		**forks;
-	t_philo		**philos;
 	
 	if (ac < 5 || ac > 6)
 		return (ft_putendl_fd(ERROR, STDERR_FILENO), 1);
-	forks = NULL;
-	philos = NULL;
 	process = ft_parse_input(ac - 1, av + 1);
 	if (!process)
 		return (ft_putendl_fd(ERROR, STDERR_FILENO), 1);
-	if (!ft_initialization(process, forks, philos))
+	if (!ft_initialization(process))
 		return 1;
 	if (!ft_threads(process))
 		return (1);
-	ft_free_forks(forks, process->number_of_philos);
-	ft_free_philos(philos, process->number_of_philos);
+	ft_free_forks(process->forks, process->number_of_philos);
+	ft_free_philos(process->philos, process->number_of_philos);
 	free(process);	
 	return (0);
 }

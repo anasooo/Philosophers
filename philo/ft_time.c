c6cdbf35d4757aec4509d9c609458b338b74f5dc
anasooo <anassodor1@gmail.com>
2024-12-22 23:21:23 +0100

    Refactor ft_time.c for code simplification

--- philo/ft_time.c
+++ philo/ft_time.c
@@ -6,32 +6,34 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 06:25:12 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/20 12:37:48 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/22 18:00:38 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h" 
+#include "philo.h"
 
 unsigned int	ft_get_time(void)
 {
-    struct timeval	time;
-    static unsigned int	start;
+	struct timeval		time;
+	static unsigned int	start;
 
-    if (!start)
-    {
-        gettimeofday(&time, NULL);
-        start = (unsigned int)(time.tv_sec * 1000 + time.tv_usec / 1000);
-        return (0);
-    }
-    gettimeofday(&time, NULL);
-    return ((unsigned long)(time.tv_sec * 1000) + (time.tv_usec / 1000) - start);
+	if (!start)
+	{
+		gettimeofday(&time, NULL);
+		start = (unsigned int)(time.tv_sec * 1000 + time.tv_usec / 1000);
+		return (0);
+	}
+	gettimeofday(&time, NULL);
+	return ((unsigned long)(time.tv_sec * 1000) + (time.tv_usec / 1000)
+		- start);
 }
-int ft_usleep(unsigned long time)
-{
-    unsigned long start;
 
-    start = ft_get_time();
-    while (ft_get_time() - start < time)
-        usleep(60);
-    return (0);
+int	ft_usleep(unsigned long time)
+{
+	unsigned long	start;
+
+	start = ft_get_time();
+	while (ft_get_time() - start < time)
+		usleep(60);
+	return (0);
 }

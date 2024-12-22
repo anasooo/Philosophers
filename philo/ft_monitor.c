97cc13debea7eefc6d751f5a61b3001174d04a79
anasooo <anassodor1@gmail.com>
2024-12-22 23:20:08 +0100

    refactor: Simplify death handling logic in ft_monitor.c

--- philo/ft_monitor.c
+++ philo/ft_monitor.c
@@ -6,46 +6,41 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:42:18 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/21 22:57:21 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/22 23:14:54 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "philo.h"        
+#include "philo.h"
+
+void	ft_handle_death(t_process *p, unsigned int *i, bool *died)
+{
+	ft_set_philo_died(p);
+	*died = true;
+	ft_print_death(p->philos[*i]);
+}
 
 int	ft_check_last_eat(t_process *p, unsigned int i)
 {
 	unsigned int	time;
-	bool	died;
+	bool			died;
 
-    pthread_mutex_lock(&p->philos[i]->mutex);
+	pthread_mutex_lock(&p->philos[i]->mutex);
 	died = false;
 	time = ft_get_time();
 	if (p->number_of_philos % 2 == 0)
-	{	if (p->time.to_die + 1 < time - p->philos[i]->last_eat )
-		{	
-     		ft_set_philo_died(p);
-			died = true;
-			pthread_mutex_lock(&p->mutex1);
-			ft_print_death(p->philos[i]);
-			pthread_mutex_unlock(&p->mutex1);
-		}
+	{
+		if (p->time.to_die + 1 < time - p->philos[i]->last_eat)
+			ft_handle_death(p, &i, &died);
 	}
-	else
-		if (p->time.to_die < time - p->philos[i]->last_eat)
-		{
-			ft_set_philo_died(p);
-			died = true;
-			pthread_mutex_lock(&p->mutex1);
-			ft_print_death(p->philos[i]);
-			pthread_mutex_unlock(&p->mutex1);
-		}
-    pthread_mutex_unlock(&p->philos[i]->mutex);
+	else if (p->time.to_die < time - p->philos[i]->last_eat)
+		ft_handle_death(p, &i, &died);
+	pthread_mutex_unlock(&p->philos[i]->mutex);
 	return ((int)died);
 }
 
 int	ft_is_died(t_process *p)
 {
-	int	i;
+	int		i;
 	bool	died;
 
 	i = 0;
@@ -69,8 +64,8 @@ void	*ft_monitor(void *arg)
 	if (!arg)
 		return (NULL);
 	p = (t_process *)arg;
-    all_is_ready(p);
-	ft_usleep(1);
+	all_is_ready(p);
+	ft_usleep(10);
 	while (true)
 	{
 		if (ft_is_died(p) != 0)

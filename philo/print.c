0fec2da0d2db10a4e0208e87a83e4dc50d246ea1
anasooo <anassodor1@gmail.com>
2024-12-22 23:22:04 +0100

    Refactor print.c for code simplification and improved readability

--- philo/print.c
+++ philo/print.c
@@ -6,32 +6,33 @@
 /*   By: asodor <asodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 02:16:26 by asodor            #+#    #+#             */
-/*   Updated: 2024/12/19 19:38:35 by asodor           ###   ########.fr       */
+/*   Updated: 2024/12/22 18:02:02 by asodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void    ft_print_fork(t_philo *philo)
+void	ft_print_fork(t_philo *philo)
 {
-    printf("%u %d %s", ft_get_time(), philo->id, FORK);
+	printf("%u %d %s", ft_get_time(), philo->id, FORK);
 }
 
-void    ft_print_eating(t_philo *philo)
+void	ft_print_eating(t_philo *philo)
 {
-    printf("%u %d %s", ft_get_time(), philo->id, EAT);
-}
-void   ft_print_sleeping(t_philo *philo)
-{
-    printf("%u %d %s", ft_get_time(), philo->id, SLEEP);
+	printf("%u %d %s", ft_get_time(), philo->id, EAT);
 }
 
-void    ft_print_thinking(t_philo *philo)
+void	ft_print_sleeping(t_philo *philo)
 {
-    printf("%u %d %s", ft_get_time(), philo->id, THINK);
+	printf("%u %d %s", ft_get_time(), philo->id, SLEEP);
 }
 
-void    ft_print_death(t_philo *philo)
+void	ft_print_thinking(t_philo *philo)
 {
-    printf("%u %d %s", ft_get_time(), philo->id, DEAD);
+	printf("%u %d %s", ft_get_time(), philo->id, THINK);
+}
+
+void	ft_print_death(t_philo *philo)
+{
+	printf("%u %d %s", ft_get_time(), philo->id, DEAD);
 }
